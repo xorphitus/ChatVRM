063bbc5857fb5180b89ca0488f24c27ed4ab095c
xorphitus <xorphitus@gmail.com>
2024-09-09 23:21:51 +0900

    ChatGPT to Ollama

--- src/features/chat/openAiChat.ts
+++ src/features/chat/openAiChat.ts
@@ -1,30 +1,6 @@
-import { Configuration, OpenAIApi } from "openai";
 import { Message } from "../messages/messages";
 
-export async function getChatResponse(messages: Message[], apiKey: string) {
-  if (!apiKey) {
-    throw new Error("Invalid API Key");
-  }
-
-  const configuration = new Configuration({
-    apiKey: apiKey,
-  });
-  // ブラウザからAPIを叩くときに発生するエラーを無くすworkaround
-  // https://github.com/openai/openai-node/issues/6#issuecomment-1492814621
-  delete configuration.baseOptions.headers["User-Agent"];
-
-  const openai = new OpenAIApi(configuration);
-
-  const { data } = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: messages,
-  });
-
-  const [aiRes] = data.choices;
-  const message = aiRes.message?.content || "エラーが発生しました";
-
-  return { message: message };
-}
+const OLLAMA_URL = "http://localhost:11434/api/chat"
 
 export async function getChatResponseStream(
   messages: Message[],
@@ -34,18 +10,15 @@ export async function getChatResponseStream(
     throw new Error("Invalid API Key");
   }
 
-  const headers: Record<string, string> = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${apiKey}`,
-  };
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
-    headers: headers,
+  // TODO temporary work around
+  const model = apiKey;
+
+  const res = await fetch(OLLAMA_URL, {
     method: "POST",
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: messages,
+      model,
+      messages,
       stream: true,
-      max_tokens: 200,
     }),
   });
 
@@ -67,7 +40,7 @@ export async function getChatResponseStream(
             .filter((val) => !!val && val.trim() !== "[DONE]");
           for (const chunk of chunks) {
             const json = JSON.parse(chunk);
-            const messagePiece = json.choices[0].delta.content;
+            const messagePiece = json.message.content;
             if (!!messagePiece) {
               controller.enqueue(messagePiece);
             }
