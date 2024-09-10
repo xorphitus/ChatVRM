019fe2959a5a9953b15854e4e4fe821e65c3d97e
xorphitus <xorphitus@gmail.com>
2024-09-10 21:03:15 +0900

    feat: emulate emotions with VOICEBOX parameters

--- src/features/speak/voicevox.ts
+++ src/features/speak/voicevox.ts
@@ -14,7 +14,62 @@ export async function fetchVoice(
 
   const queryJson = await query.json();
 
+  const params = styleToParameters(style);
+  queryJson["speedScale"] = params.speedScale;
+  queryJson["pitchScale"] = params.pitchScale;
+  queryJson["intonationScale"] = params.intonationScale;
+
   return synthesizeVoice(speaker, queryJson);
+}
+
+function styleToParameters(style: TalkStyle) {
+  let parameters = {
+    speedScale: 1.05,
+    pitchScale: 0,
+    intonationScale: 1,
+  };
+
+  switch (style) {
+    case "angry":
+        parameters = {
+            speedScale: 1,
+            pitchScale: -0.15,
+            intonationScale: 0.8,
+        };
+        break;
+    case "fear":
+        parameters = {
+            speedScale: 1.3,
+            pitchScale: 0.12,
+            intonationScale: 1.5,
+        };
+        break;
+    case "surprised":
+        parameters = {
+            speedScale: 1.3,
+            pitchScale: 0.1,
+            intonationScale: 1.5,
+        };
+        break;
+    case "happy":
+        parameters = {
+            speedScale: 1.1,
+            pitchScale: 0.07,
+            intonationScale: 1.3,
+        };
+        break;
+    case "sad":
+        parameters = {
+            speedScale: 0.9,
+            pitchScale: -0.15,
+            intonationScale: 0.9,
+        };
+        break;
+    default:
+      break;
+  }
+
+  return parameters;
 }
 
 async function queryVoice(message: string, speaker: number) {
