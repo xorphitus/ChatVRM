ea64dee262212edf5c64342ae1c42437a5a2754d
xorphitus <xorphitus@gmail.com>
2024-09-10 09:33:06 +0900

    refactor: follow linter

--- src/components/githubLink.tsx
+++ src/components/githubLink.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { buildUrl } from "@/utils/buildUrl";
 
 export const GitHubLink = () => {
@@ -10,12 +11,12 @@ export const GitHubLink = () => {
         target="_blank"
       >
         <div className="p-8 rounded-16 bg-[#1F2328] hover:bg-[#33383E] active:bg-[565A60] flex">
-          <img
+          <Image
             alt="https://github.com/pixiv/ChatVRM"
             height={24}
             width={24}
             src={buildUrl("/github-mark-white.svg")}
-          ></img>
+          />
           <div className="mx-4 text-white font-M_PLUS_2 font-bold">Fork me</div>
         </div>
       </a>
