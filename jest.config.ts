b40a7ad7e5c978f86d86d8d3cd78762b2121527b
xorphitus <xorphitus@gmail.com>
2024-09-19 11:02:25 +0900

    chore: jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+    verbose: true,
+    testEnvironment: "node",
+    transform: {
+        "^.+.tsx?$": ["ts-jest",{}],
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+};
+
+export default config;
