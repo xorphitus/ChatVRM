e8c261cd9effaf5419446889617e2c256671bc07
xorphitus <xorphitus@gmail.com>
2024-10-05 09:53:52 +0900

    Revert "refactor"
    This reverts commit c36494142054fbeab6903bb4ab9d6f3a46fcdf64.

--- __tests__/features/messages/messages.test.ts
+++ __tests__/features/messages/messages.test.ts
@@ -1,8 +1,2 @@
-import { parseMessage } from "@/features/messages/messages";
-
-test("parseMessage", () => {
-  expect(parseMessage("[angry]こんにちは")).toEqual({
-    tag: "[angry]",
-    body: "こんにちは",
-  });
+test("test", () => {
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,6 @@ import {
   Message,
   textsToScreenplay,
   Screenplay,
-  parseMessage,
-  trimUnpronounceableCharacters,
-  matchSentence,
 } from "@/features/messages/messages";
 import { speakCharacter } from "@/features/messages/speakCharacter";
 import { MessageInputContainer } from "@/components/messageInputContainer";
@@ -121,6 +118,7 @@ export default function Home() {
       const reader = stream.getReader();
       let receivedMessage = "";
       let aiTextLog = "";
+      let tag = "";
       const sentences = new Array<string>();
       try {
         while (true) {
@@ -130,35 +128,43 @@ export default function Home() {
           receivedMessage += value;
 
           // 返答内容のタグ部分の検出
-          const parsed = parseMessage(receivedMessage);
-          const tag = parsed.tag;
-          if (tag) {
-            receivedMessage = parsed.body;
+          const tagMatch = receivedMessage.match(/^\[(.*?)]/);
+          if (tagMatch && tagMatch[0]) {
+            tag = tagMatch[0];
+            receivedMessage = receivedMessage.slice(tag.length);
           }
 
           // 返答を一文単位で切り出して処理する
-          let sentence = matchSentence(receivedMessage);
-          if (!sentence) {
-            continue;
+          const sentenceMatch = receivedMessage.match(
+            /^(.+[。．！？\n]|.{10,}[、,])/,
+          );
+          if (sentenceMatch && sentenceMatch[0]) {
+            const sentence = sentenceMatch[0];
+            sentences.push(sentence);
+            receivedMessage = receivedMessage
+              .slice(sentence.length)
+              .trimStart();
+
+            // 発話不要/不可能な文字列だった場合はスキップ
+            if (
+              !sentence.replace(
+                /^[\s\[({「［（【『〈《〔｛«‹〘〚〛〙›»〕》〉』】）］」})\]]+$/g,
+                "",
+              )
+            ) {
+              continue;
+            }
+
+            const aiText = `${tag} ${sentence}`;
+            const aiTalks = textsToScreenplay([aiText], voicevoxParam);
+            aiTextLog += aiText;
+
+            // 文ごとに音声を生成 & 再生、返答を表示
+            const currentAssistantMessage = sentences.join(" ");
+            handleSpeakAi(aiTalks[0], () => {
+              setAssistantMessage(currentAssistantMessage);
+            });
           }
-          sentences.push(sentence);
-          receivedMessage = receivedMessage.slice(sentence.length).trimStart();
-
-          // 発話不要/不可能な文字列だった場合はスキップ
-          const trimmed = trimUnpronounceableCharacters(sentence);
-          if (!trimmed) {
-            continue;
-          }
-
-          const aiText = `${tag} ${trimmed}`;
-          const aiTalks = textsToScreenplay([aiText], voicevoxParam);
-          aiTextLog += aiText;
-
-          // 文ごとに音声を生成 & 再生、返答を表示
-          const currentAssistantMessage = sentences.join(" ");
-          handleSpeakAi(aiTalks[0], () => {
-            setAssistantMessage(currentAssistantMessage);
-          });
         }
       } catch (e) {
         setChatProcessing(false);
